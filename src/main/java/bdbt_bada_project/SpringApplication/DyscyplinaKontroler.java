c93711edff062e875f0d4a4baf5cb9d7ba0b8d12
Jakub Strzelczyk <01178695@pw.edu.pl>
2024-01-04 23:41:35 +0100

    Commit J.S

--- src/main/java/bdbt_bada_project/SpringApplication/DyscyplinaKontroler.java
+++ src/main/java/bdbt_bada_project/SpringApplication/DyscyplinaKontroler.java
@@ -2,9 +2,11 @@ package bdbt_bada_project.SpringApplication;
 
 import jdk.jfr.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -19,6 +21,18 @@ public class DyscyplinaKontroler {
         model.addAttribute("listaDyscyplin", listaDyscyplin);
 
         return "dyscypliny";
+    }
+
+    @GetMapping("/dyscypliny/dodaj")
+    public String showDyscyplinaNewForm(Model model){
+        model.addAttribute("dyscyplina", new Dyscyplina());
+        return "dyscyplina_dodaj";
+    }
+
+    @PostMapping("/dyscypliny/zapisz")
+    public String zapiszDyscyplinę(Dyscyplina dyscyplina){
+        repo.save(dyscyplina);
+        return "redirect:/dyscypliny";
     }
 
 }
